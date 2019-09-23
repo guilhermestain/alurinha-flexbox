931c3707506c1b30bd15fecc8f69f4b40b684557
claytonbatista <guilhermerealponto@gmail.com>
2019-09-23 16:43:55 -0300

    FIX

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ if('serviceWorker' in navigator){
   window.addEventListener('load', ()=>{
     navigator.serviceWorker.register('/sw.js')
     .then((reg)=>{
-      console.log('Registrado nosso primeiro service worker', req);
+      console.log('Registrado nosso primeiro service worker', reg);
     }).catch((err)=>{
       console.log('Algo de errado aconteceu', err);
     })
